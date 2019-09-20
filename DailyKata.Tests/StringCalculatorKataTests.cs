85905bcfcb717596cf371f87c288a6e74025c9cb
Andrei Ivascu <andrew.ivascu@gmail.com>
2019-09-20 16:29:56 +0300

    Implements the string calculator kata setup

---
+++ DailyKata.Tests/StringCalculatorKataTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+
+namespace DailyKata.Tests
+{
+    public class StringCalculatorKataTests
+    {
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("1", 1)]
+        [InlineData("-32", -32)]
+        public void Calculate_SingleNumber_ReturnsSingleNumber(string input, int expected)
+        {
+            var actual = StringCalculatorKata.Calculate(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("2,3", 5)]
+        [InlineData("2,-9", -7)]
+        public void Calculate_TwoNumbers_ReturnsSumOfNumbers(string input, int expected)
+        {
+            var actual = StringCalculatorKata.Calculate(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Calculate_EmptyString_ThrowsException()
+        {
+            Action act = () => StringCalculatorKata.Calculate(string.Empty);
+
+            Assert.Throws<ArgumentException>("input", act);
+        }
+
+        [Fact]
+        public void Calculate_NullString_ThrowsException()
+        {
+            Action act = () => StringCalculatorKata.Calculate(default(string));
+
+            Assert.Throws<ArgumentNullException>("input", act);
+        }
+    }
+}
---
+++ DailyKata/Katas/StringCalculatorKata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyKata
+{
+    public class StringCalculatorKata
+    {
+        public static int Calculate(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
