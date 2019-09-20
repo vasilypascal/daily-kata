803667bd39ce5fe030223c830439f648bb51f945
Andrei Ivascu <andrew.ivascu@gmail.com>
2019-09-20 10:04:14 +0300

    initial commit

---
+++ DailyKata/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DailyKata
+{
+    public class Kata
+    {
+        public static string FindNeedle(object[] haystack)
+        {
+            //Code goes here!
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DailyKata/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyKata
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
