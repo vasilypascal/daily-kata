89dad400f0f236d22ccd25c13bc496dbfbc24557
Andrei Ivascu <andrew.ivascu@gmail.com>
2019-09-25 12:24:12 +0300

    Implemented the polymorphic refactoring kata

---
+++ DailyKata.Tests/PolymorphicRefactoringKataTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DailyKata.Katas;
+using Xunit;
+
+namespace DailyKata.Tests
+{
+    public class PolymorphicRefactoringKataTests
+    {
+        [Theory]
+        [InlineData(JobTitle.Junior, 100)]
+        [InlineData(JobTitle.Middle, 175)]
+        [InlineData(JobTitle.Senior, 195)]
+        [InlineData(JobTitle.Consultant, 215)]
+        public void GetSalary_ForGrade_ExpectedSalary(JobTitle jobTitle, double expected)
+        {
+            var sut = new PolymorphicRefactoringKata(jobTitle);
+
+            var actual = sut.GetSalary();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetSalary_ForUnknownGrade_ThrowsException()
+        {
+            var sut = new PolymorphicRefactoringKata(JobTitle.Unknown);
+
+            Action act = () => sut.GetSalary();
+
+            Assert.Throws<InvalidOperationException>(act);
+        }
+
+        [Fact]
+        public void GetSalary_ForUnknownGrade_ThrowsExceptionWithMessage()
+        {
+            var sut = new PolymorphicRefactoringKata(JobTitle.Unknown);
+
+            var exception = Record.Exception(() => sut.GetSalary());
+
+            Assert.Equal("Invalid job title", exception.Message);
+        }
+    }
+}
---
+++ DailyKata/Katas/PolymorphicRefactoringKata.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DailyKata.Katas
+{
+    public class PolymorphicRefactoringKata
+    {
+        private readonly JobTitle _jobTitle;
+
+        public PolymorphicRefactoringKata(JobTitle jobTitle)
+        {
+            _jobTitle = jobTitle;
+        }
+
+        public double GetSalary()
+        {
+            switch (_jobTitle)
+            {
+                case JobTitle.Junior:
+                    return GetBaseSalary();
+
+                case JobTitle.Middle:
+                    return GetBaseSalary() + 75;
+
+                case JobTitle.Senior:
+                    return GetBaseSalary() + 75 + 20;
+
+                case JobTitle.Consultant:
+                    return GetBaseSalary() + 75 + 25 + 15;
+
+                default:
+                    throw new InvalidOperationException("Invalid job title");
+            }
+        }
+
+        private double GetBaseSalary()
+        {
+            return 100;
+        }
+    }
+
+    public enum JobTitle
+    {
+        Unknown = 0,
+        Junior,
+        Middle,
+        Senior,
+        Consultant
+    }
+}
